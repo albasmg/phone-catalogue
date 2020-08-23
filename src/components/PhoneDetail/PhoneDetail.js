7cbc304f3a2f877052a22c134e131788bb0acad3
Alba San Martin <alba.sanmartin.g@gmail.com>
2020-08-23 17:12:36 +0200

    add phone details styles and responsive

--- src/components/PhoneDetail/PhoneDetail.js
+++ src/components/PhoneDetail/PhoneDetail.js
@@ -12,17 +12,36 @@ const PhoneDetails = ({
   ram,
 }) => {
   return (
-    <section>
-      <div>
-        <img src={mapPhoneImages[imageFileName]} alt={name}></img>
+    <section className="phoneDetails">
+      <div className="phoneDetails__imgContainer">
+        <img
+          src={mapPhoneImages[imageFileName]}
+          alt={name}
+          className="phoneDetails__img"
+        ></img>
       </div>
-      <h2>{name}</h2>
-      <h3>{manufacturer}</h3>
-      <p>{description}</p>
-      <p>{color}</p>
-      <p>{screen}</p>
-      <p>{processor}</p>
-      <p>{ram}</p>
+      <div className="phoneDetails__info">
+        <h2 className="phoneDetails__name">{name}</h2>
+        <h3 className="phoneDetails__manufacturer"> {manufacturer}</h3>
+        <p className="phoneDetails__description">{description}</p>
+        <p className="phoneDetails__extraDetails">
+          {' '}
+          <span>Color: </span>
+          {color}
+        </p>
+        <p className="phoneDetails__extraDetails">
+          <span>Screen: </span>
+          {screen}
+        </p>
+        <p className="phoneDetails__extraDetails">
+          <span>Processor: </span>
+          {processor}
+        </p>
+        <p className="phoneDetails__extraDetails">
+          <span>Ram: </span>
+          {ram}
+        </p>
+      </div>
     </section>
   );
 };
